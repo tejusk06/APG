93a4392a91dc67938c9ab96191f22cfbee40cce3
tejusk06 <tejus.kabadi@gmail.com>
2022-05-08 10:06:33 +0530

    added membertsack on ready

--- classes.js
+++ classes.js
@@ -1,1 +1,17 @@
 console.log("loggin from github pages");
+
+MemberStack.onReady.then(function (member) {
+  // check if member is logged in
+  // returns true or false
+
+  if (!member.loggedIn) {
+    //   If member is not logged in redirect to main page
+    window.location.replace(window.location.hostname);
+  } else {
+    // If member is logged in then do this logic
+    const airtableID = member["AirtableID"];
+    console.log("airtable id is ", airtableID);
+  }
+
+  // do things with the member object
+});
