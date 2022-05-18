209b1a10c580731cd5782e7816ab91c66d91acbe
tejusk06 <tejus.kabadi@gmail.com>
2022-05-19 00:49:43 +0530

    query select to all

--- teacherClasses.js
+++ teacherClasses.js
@@ -35,7 +35,9 @@ MemberStack.onReady.then(function (member) {
         upcomingClassDiv.querySelector(".teacher-name").innerHTML = `${upcomingClassData.teacherName}`;
         upcomingClassDiv.querySelector(".topics-text").innerHTML = `${upcomingClassData.classTopics}`;
         upcomingClassDiv.querySelector(".homework-text").innerHTML = `${upcomingClassData.classTopics}`;
-        upcomingClassDiv.querySelector(".view-class-button").href = `/teacher/class/:${upcomingClassData.classID}`;
+        upcomingClassDiv.querySelectorAll(
+          ".view-class-button"
+        )[0].href = `/teacher/class/:${upcomingClassData.classID}`;
 
         // Appending the upcoming class Div
         classesHolder.appendChild(upcomingClassDiv);
