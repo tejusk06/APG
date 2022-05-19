f7591572282280aa91d931d349bd554672c9031e
tejusk06 <tejus.kabadi@gmail.com>
2022-05-19 21:01:43 +0530

    added classes href

--- adminIndividualStudentClasses.js
+++ adminIndividualStudentClasses.js
@@ -9,6 +9,8 @@ MemberStack.onReady.then(function (member) {
   const studentID = classCourse.split("&")[0].split("=")[1];
   const courseID = classCourse.split("&")[1].split("=")[1];
 
+  document.querySelector(".classTemplates").style.display = "none";
+
   console.log("student ID", studentID);
   console.log("course ID", courseID);
 
@@ -20,12 +22,19 @@ MemberStack.onReady.then(function (member) {
   fetch(`https://apguru-server.herokuapp.com/api/v1/admin/student/${studentID}`)
     .then((response) => response.json())
     .then((response) => {
-      document.querySelector(".student-image").src = response.student.image;
+      if (response.student.image) {
+        document.querySelector(".student-image").src = response.student.image;
+      }
       document.querySelector(".student-name-heading").innerHTML = response.student.name;
       document.querySelector(".student-subheading").innerHTML = response.student.email;
       document.querySelector(
-        ".add-class-button"
+        ".add-classes-button"
       ).href = `https://web.miniextensions.com/obWVQWXI9bSHZEhLEug5/${studentID}`;
       document.querySelector(".student-name-heading").innerHTML = response.student.name;
     });
+
+  const classesHolder = document.querySelectorAll(".classes-holder")[0];
+  const upcomingTemplate = document.querySelectorAll(".class-wrap.upcoming")[0];
+  const completedTemplate = document.querySelectorAll(".class-wrap.completed")[0];
+  const missedTemplate = document.querySelectorAll(".class-wrap.missed")[0];
 });
