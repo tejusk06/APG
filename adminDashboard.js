ffe33ca77701c4edd8dc43604bc8b0ef1bc6b617
tejusk06 <tejus.kabadi@gmail.com>
2022-05-20 14:30:56 +0530

    added admin dashboard logic

---
+++ adminDashboard.js
@@ -0,0 +1,19 @@
+console.log("Admin Dashboard logic");
+// Logic for Class form embed is in the webflow page below attendance
+
+MemberStack.onReady.then(function (member) {
+  //   If member is not logged in redirect to main page
+
+  if (!member.loggedIn) {
+    window.location.replace(window.location.hostname);
+  }
+
+  //   Making the api call to get student data from students table
+  fetch(`https://apguru-server.herokuapp.com/api/v1/admin/dashboard`)
+    .then((response) => response.json())
+    .then((response) => {
+      document.querySelector(".upcoming-classes").innerHTML = response.stats.upcomingClasses;
+      document.querySelector(".completed-classes").innerHTML = response.stats.completedClasses;
+      document.querySelector(".total-students").innerHTML = response.stats.totalStudents;
+    });
+});
