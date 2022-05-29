5d54fb977587e0f3bd29bc9ba5427d184c20e8a1
tejusk06 <tejus.kabadi@gmail.com>
2022-05-29 16:19:06 +0530

    removed student classes button href

--- adminIndividualStudentClasses.js
+++ adminIndividualStudentClasses.js
@@ -26,9 +26,6 @@ MemberStack.onReady.then(function (member) {
       }
       document.querySelector(".student-name-heading").innerHTML = response.student.name;
       document.querySelector(".student-subheading").innerHTML = response.student.email;
-      document.querySelector(
-        ".add-classes-button"
-      ).href = `https://web.miniextensions.com/obWVQWXI9bSHZEhLEug5/${studentID}`;
 
       document.querySelector("#student-tests").href = `/admin/student-tests?studentID=${studentID}`;
       document.querySelector("#student-classes").href = `#`;
