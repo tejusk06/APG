2ffb0b89f1392f2e5b097f5aedf48752b968fb03
tejusk06 <tejus.kabadi@gmail.com>
2022-05-30 01:03:09 +0530

    added total topics stat

--- studentSatDashboard.js
+++ studentSatDashboard.js
@@ -17,9 +17,14 @@ MemberStack.onReady.then(function (member) {
 
       document.querySelector(".upcoming-classes-stat").innerHTML = response.stats.upcomingClasses;
       document.querySelector(".all-classes-stat").innerHTML = response.stats.allClasses;
-      document.querySelector(".math-topics-stat").innerHTML = response.stats.mathTopicsCompleted;
-      document.querySelector(".reading-topics-stat").innerHTML = response.stats.readingTopicsCompleted;
-      document.querySelector(".writing-topics-stat").innerHTML = response.stats.writingTopicsCompleted;
+      if (member.membership.name == "AP Guru SAT Students") {
+        document.querySelector(".math-topics-stat").innerHTML = response.stats.mathTopicsCompleted;
+        document.querySelector(".reading-topics-stat").innerHTML = response.stats.readingTopicsCompleted;
+        document.querySelector(".writing-topics-stat").innerHTML = response.stats.writingTopicsCompleted;
+      } else {
+        document.querySelector(".completed-topics-stat").innerHTML = response.stats.totalTopicsCompleted;
+      }
+
       document.querySelector(".pending-homework-stat").innerHTML = response.stats.homeworkPending;
       document.querySelector(".due-homework-stat").innerHTML = response.stats.homeworkDue;
       document.querySelector(".completed-homework-stat").innerHTML = response.stats.homeworkCompleted;
