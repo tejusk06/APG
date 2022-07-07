68fef02ab957b54adc43692536ecdf38618d5aed
tejusk06 <tejus.kabadi@gmail.com>
2022-07-07 23:09:34 +0530

    rearranging topics to show completed first8

--- studentTopics.js
+++ studentTopics.js
@@ -92,7 +92,7 @@ MemberStack.onReady.then(function (member) {
 
       // giving a timeout so the filtering happens first then the topics get rearranged
       console.log("setting timeout");
-      setTimeout(rearrangeTopics(), 500);
+      setTimeout(rearrangeTopics(), 5000);
     };
   });
 });
