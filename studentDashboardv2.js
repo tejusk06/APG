be75e33d56dc1ba64b1a5d1fb7f52333ce24b616
tejusk06 <tejus.kabadi@gmail.com>
2022-07-13 08:44:06 +0530

    changed url to dashboard v2

--- studentDashboardv2.js
+++ studentDashboardv2.js
@@ -10,7 +10,7 @@ MemberStack.onReady.then(function (member) {
   const studentAirtableID = member["airtableid"];
 
   //   Making the api call to get student stats from students table
-  fetch(`https://apguru-server.herokuapp.com/api/v1/student/dashboard/${studentAirtableID}`)
+  fetch(`https://apguru-server.herokuapp.com/api/v1/student/dashboard-v2/${studentAirtableID}`)
     .then((response) => response.json())
     .then((response) => {
       console.log("response", response);
@@ -20,6 +20,7 @@ MemberStack.onReady.then(function (member) {
       //   document.querySelector(".all-classes-stat").innerHTML = response.stats.allClasses;
       if (member.membership.name == "AP Guru SAT Students") {
         document.querySelector(".math-topics-stat").innerHTML = response.stats.mathTopicsCompleted;
+        document.querySelectorAll(".math-topics-stat")[1].innerHTML = response.stats.mathTopicsCompleted;
         document.querySelector(".reading-topics-stat").innerHTML = response.stats.readingTopicsCompleted;
         document.querySelector(".writing-topics-stat").innerHTML = response.stats.writingTopicsCompleted;
       } else {
