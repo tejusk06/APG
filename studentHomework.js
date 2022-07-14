6cd6aef3c3037f296ba3f1cfb46bc40932220a9f
tejusk06 <tejus.kabadi@gmail.com>
2022-07-15 00:40:00 +0530

    added logic to update page for homework

--- studentHomework.js
+++ studentHomework.js
@@ -92,9 +92,21 @@ MemberStack.onReady.then(function (member) {
 
                 if (isPast) {
                   hwItem.querySelector(".hw-due").style.display = "flex";
-                  hwItem.querySelector(
-                    ".hw-due"
-                  ).href = `https://web.miniextensions.com/p9ejiPufAv3sWKtq87oe/${eachHomework.homeworkId}`;
+                  // hwItem.querySelector(
+                  //   ".hw-due"
+                  // ).href = `https://web.miniextensions.com/p9ejiPufAv3sWKtq87oe/${eachHomework.homeworkId}`;
+                  hwItem.querySelector(".hw-due").addEventListener("click", () => {
+                    localStorage.setItem("formName", "Update Homework");
+                    localStorage.setItem(
+                      "formLink",
+                      `https://web.miniextensions.com/p9ejiPufAv3sWKtq87oe/${eachHomework.homeworkId}`
+                    );
+                    localStorage.setItem("pageName", "All Homework");
+                    localStorage.setItem("pageLink", `${window.location.href}`);
+
+                    window.location.replace("/update-page");
+                  });
+
                   hwItem.querySelector(".hw-due-date").innerHTML = eachHomework.momentDate;
                   hwItem.querySelector(".homework-status").innerHTML = "due";
                 } else {
