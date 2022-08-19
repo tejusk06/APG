e231212067e695f53a35c845dc1e137ff4140f60
tejusk06 <tejus.kabadi@gmail.com>
2022-08-19 05:34:23 +0530

    updated url for stunt tests

--- adminIndividualStudentTests.js
+++ adminIndividualStudentTests.js
@@ -47,7 +47,7 @@ MemberStack.onReady.then(function (member) {
       document.querySelector("#student-tests").classList.add("w--current");
       document.querySelector(
         "#student-classes"
-      ).href = `/admin/student-classes/?studentID=${response.student.id}&courseID=${response.student.courseID}`;
+      ).href = `/coordinator-admin/student-classes/?studentID=${response.student.id}&courseID=${response.student.courseID}`;
     });
 
   //   Making the api call to get tests data for the student
