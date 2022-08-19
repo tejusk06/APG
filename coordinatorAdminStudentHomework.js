b3e5fcb28f121e8ad148de647561178260062eb6
tejusk06 <tejus.kabadi@gmail.com>
2022-08-19 13:26:07 +0530

    added homework name capital case

--- coordinatorAdminStudentHomework.js
+++ coordinatorAdminStudentHomework.js
@@ -63,7 +63,10 @@ MemberStack.onReady.then(function (member) {
       const today = new Date();
 
       assignedHomework.forEach((eachHomework) => {
-        const homeworkName = eachHomework.split("-")[0].toUpperCase() + eachHomework.split("-")[1];
+        const homeworkName =
+          eachHomework.split("-")[0].toUpperCase() +
+          eachHomework.split("-")[1].charAt(0).toUpperCase() +
+          eachHomework.split("-")[1].slice(1);
 
         const homeworkItemDiv = homeworkItem.cloneNode(true);
         if (eachHomework.completed) {
