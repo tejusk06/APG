a72e417c641dbf7305487704f2a32e67bb56e9ff
tejusk06 <tejus.kabadi@gmail.com>
2022-08-20 09:23:27 +0530

    removed console logs

--- coordinatorAdminStudentClasses.js
+++ coordinatorAdminStudentClasses.js
@@ -66,9 +66,6 @@ MemberStack.onReady.then(function (member) {
         upcomingClassDiv.querySelector(".class-name").innerHTML = `${upcomingClassData.className}`;
         upcomingClassDiv.querySelector(".teacher-name").innerHTML = upcomingClassData.teacherName;
 
-        console.log("teacher element", upcomingClassDiv.querySelector(".teacher-name"));
-        console.log("teacher teacher name", upcomingClassData.teacherName);
-
         upcomingClassDiv.querySelector(".location-text").innerHTML = upcomingClassData.location
           ? upcomingClassData.location
           : "";
